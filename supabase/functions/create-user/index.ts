17f9be47f1079b0d09ff4e2f5c99370c9b08bc01
Skip <skip@usecurling.com>
2026-03-15 16:34:23 +0000

    feat: Implement Third-Party Management module

--- supabase/functions/create-user/index.ts
+++ supabase/functions/create-user/index.ts
@@ -1,6 +1,6 @@
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts'
 import { createClient } from 'npm:@supabase/supabase-js@2.39.3'
-import { corsHeaders } from 'shared/cors.ts'
+import { corsHeaders } from '../_shared/cors.ts'
 
 Deno.serve(async (req: Request) => {
   if (req.method === 'OPTIONS') {
